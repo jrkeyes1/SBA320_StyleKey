ad7e1fea2e21b3bd3bdc59cffbf22a635727fcbb
jrkeyes1 <jrkeyes1@gmail.com>
2024-08-16 22:27:49 -0400

    added home

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to StyleKey</h1>
+      <p>Your virtual fashion closet inspired by the 'Clueless' concept.</p>
+    </div>
+  );
+};
+
+export default Home;
