10f90a278d0084803bdc7ac6eb73f5650fb8263e
jrkeyes1 <jrkeyes1@gmail.com>
2024-08-19 11:33:11 -0400

    added closet.jsx

---
+++ src/components/Closet.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const Closet = () => {
+  const [closet, setCloset] = useState([]);
+
+  useEffect(() => {
+    const savedCloset = JSON.parse(localStorage.getItem('closet')) || [];
+    setCloset(savedCloset);
+  }, []);
+
+  return (
+    <div>
+      <h1>Your Closet</h1>
+      <div className="images-grid">
+        {closet.map((image, index) => (
+          <div key={index} className="image-item">
+            <img src={image.urls.small} alt={image.alt_description} />
+            <p>{image.description || "No description"}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Closet;
+git 
